e9ea4943a1dd85667a05481273a56b75fac7ba63
onthiagooliveira <108494188+onthiagooliveira@users.noreply.github.com>
2023-01-23 13:06:35 -0300

    Creating the main file in pyhton

---
+++ app.py
@@ -0,0 +1,14 @@
+'''
+    Write a program that receives a number of days entered by the user and calculate how many minutes this amount of days has.
+'''
+
+from SubAlgoritmosPythonTutorialForBeginners import *
+
+print('Days to minutes converter')
+days = int(input('Number of days:\n'))
+
+# Function that converts days to hours
+hours = converterDaysTime(days)
+
+# Function that converts hours to minutes
+minutes = converterHoursToMinutes(hours)
