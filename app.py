c5931ebda6d55febd40b8872b0d759c06a63eafc
onthiagooliveira <onthiagooliveira@outlook.com>
2023-01-24 23:51:16 -0300

    renamed from subalgorithm_Library.py

--- app.py
+++ app.py
@@ -7,15 +7,9 @@ from subalgorithm_Library import *
 print('Days to minutes converter')
 user_input = input('Number of days:\n')
 
-#Checking if the entered data is a number valid
-if user_input.isdigit():
-    user_input_number = int(user_input)
+validate_and_execute(user_input)
 
-    # Function that converts days to hours
-    hours = converterDaysTime(user_input_number)
-    print(hours)
 
-    # Function that converts hours to minutes
 
 
 
--- subalgorithm_Library.py
+++ subalgorithm_Library.py
@@ -1,3 +1,20 @@
+# Function that validates data entered by the user
+
+def validate_and_execute(user_input):
+    # Checking if the entered data is a number valid
+    if user_input.isdigit():
+        user_input_number = int(user_input)
+
+        # Function that converts days to hours
+        hours = converterDaysTime(user_input_number)
+        print(hours)
+
+        # Function that converts hours to minutes
+
+    else:
+        print('Your entered nis not a valid number.')
+
+
 
 # Function that validates and converts days to hours
 def converterDaysTime(days):
@@ -5,9 +22,6 @@ def converterDaysTime(days):
         return f'{days} days are {days * 24} hours.'
     elif days == 0:
         return f'You entered a 0, please enter a valid number'
-    else:
-        return f'Invalid value. You entered a negative value.'
-
 
 
 # Funtion that converts hours to minutes
