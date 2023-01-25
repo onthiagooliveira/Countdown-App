ccb75ffeca32fcb89404d0efef0a056f593f5ce9
onthiagooliveira <onthiagooliveira@outlook.com>
2023-01-25 14:27:57 -0300

    Creating command for lists

--- main.py
+++ main.py
@@ -1,30 +1,45 @@
+"""
+    Write a program that receives a number of days entered by the user and calculate how many minutes this amount of days has.
+"""
+
+# subalgorithm_Library
+
 # Function that validates data entered by the user
-def validate_and_execute(user_input):
+def validate_and_execute():
     # Checking if the entered data is a number valid
     try:
-
-        user_input_number = int(user_input)
+        user_input_number = int(num_of_days_element)
         if user_input_number > 0:
             calculated_value = convert_days_to_hours(user_input_number)
             print(calculated_value)
-
         elif user_input_number == 0:
             print('You entered a 0, please enter a positive number')
-
         else:
             print('It is not possible to convert, you entered a negative number.')
-
     except ValueError:
         print('Your entered is not a valid number.')
-
 
 
 # Function that validates and converts days to hours
 def convert_days_to_hours(days):
     return f'{days} days are {days * 24} hours.'
 
+
 # Funtion that converts hours to minutes
 def convert_hours_to_minutes(hours):
     return f'{hours} hours are {hours * 60} minutes.'
 
 
+
+
+# Running Aplication
+
+# Variable
+user_input = ''
+
+while user_input != 'exit':
+    user_input = input('Enter a number of days to be converted to minutes:\n')
+
+    for num_of_days_element in user_input.split(","):
+        # Validating the input e running the program
+        validate_and_execute()
